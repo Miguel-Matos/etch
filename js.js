467860c798b60d51722140f01f12d8365ed59c27
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-02-02 18:35:08 +0900

    TODO add button layout functionality

--- js.js
+++ js.js
@@ -9,7 +9,7 @@ let count = 1;
 let size = (500 / num) + "px";
 console.log(size);
 
-
+let sColor = "black";
 
 function gen (amount) {
     for (let i = 0; i < amount; i++) {
@@ -27,9 +27,39 @@ function gen (amount) {
     }
     const color = document.querySelectorAll(".square");
 
+    const red = document.querySelector("#red");
+    red.addEventListener("click", () => {
+        sColor = "red";
+    });
+
+    const blue = document.querySelector("#blue");
+    blue.addEventListener("click", () => {
+        sColor = "blue";
+    });
+
+    const green = document.querySelector("#green");
+    green.addEventListener("click", () => {
+        sColor = "green";
+    });
+
+    const yellow = document.querySelector("#yellow");
+    yellow.addEventListener("click", () => {
+        sColor = "yellow";
+    });
+
+    const white = document.querySelector("#white");
+    white.addEventListener("click", () => {
+        sColor = "white";
+    });
+
+    const black = document.querySelector("#black");
+    black.addEventListener("click", () => {
+        sColor = "black";
+    });
+
     color.forEach((colors) => {
         colors.addEventListener("mouseover", () => {
-            colors.style.backgroundColor = "black";
+            colors.style.backgroundColor = sColor;
         });
     });
     
